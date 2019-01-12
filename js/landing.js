f4f9d1b0d22ef123ea4da0a73218605263c50730
ocanty <git@ocanty.com>
2019-01-12 13:17:24 +0000

    Documented landing.js with comments

--- js/landing.js
+++ js/landing.js
@@ -2,18 +2,15 @@
 class MapView {
     constructor(canvasElement) {
         this.canvas = canvasElement
-        // hide the canvas until asset loading completes
-        // this.canvas.style.display = 'none'
-        this.ctx    = this.canvas.getContext('2d')
+        this.ctx  = this.canvas.getContext('2d')
 
         this.assetsLoaded = false
         this.shouldDraw = false
-        this.stitchedImage = new Image()
     }   
 
     loadAssets() {
         /**
-         * Our end goal is to get a stitched image of a map view
+         * Our end goal is to get a stitched image of a map view on the canvas
          * We will then be able to transform it as we wish
          * 
          * Minecraft Dynmap saves map tiles in a regular pattern 
@@ -29,57 +26,54 @@ class MapView {
         // Each tile is 128x128
    
         // screen res:
-
-        // ideally we want like 2x as many tiles as screen res
-        // the *2 is the zoom level 
+        // the *2 is due to zoom level 
         let tiles_width = (4096/128)*2
         let tiles_height = (2048/128)*2
 
-
         // note again zoom level 2
-        this.tilesDrawWidth = (tiles_width*128)/2
-        this.tilesDrawHeight = (tiles_height*128)/2
-
-
-        // we're gonna keep the camera focused on the center of the tile image
-        this.currentPoint = {
-            x: this.tilesDrawWidth / 2,
-            y: this.tilesDrawHeight / 2
+        this.stitchedTilesSize = {
+            x: (tiles_width*128)/2,
+            y: (tiles_height*128)/2
         }
 
-        
-        // so we can draw each of the tiles onto the canvas and stitch them
-        this.canvas.width = this.tilesDrawWidth
-        this.canvas.height = this.tilesDrawHeight
+        // we're gonna keep the camera focused on the center of all the tiles we get
+        this.currentPoint = {
+            x: this.stitchedTilesSize.x / 2,
+            y: this.stitchedTilesSize.y / 2
+        }
 
-        // the top left, bottom right in tile coordinates
+        // so we can draw each of the tiles onto the canvas and stitch them
+        this.canvas.width = this.stitchedTilesSize.x
+        this.canvas.height = this.stitchedTilesSize.y
+
+        // the top left, bottom right in tile coordinates, our centre point is (100,38)
         let topleft_x = 100 - (tiles_width/2)
         let topleft_y = 38 + (tiles_height/2)
 
         let bottomright_x = 100 + (tiles_width/2)
         let bottomright_y = 38 - (tiles_height/2)
  
-
         for(let y = topleft_y; y >= bottomright_y; y -= 2) {
             for(let x = topleft_x; x <= bottomright_x; x += 2) {
 
                 // keep it centered while loading (i.e if people resize their browser)
                 this.cvs = this.getCurrentViewportSize()
-                this.canvas.style.top = `-${(this.tilesDrawHeight/2) - (this.cvs.y/2)}px`
-                this.canvas.style.left = `-${(this.tilesDrawWidth/2) - (this.cvs.x/2)}px`
-        
-
-                // console.log(x,y,bottomright_y)
+                this.canvas.style.top = `-${(this.stitchedTilesSize.y/2) - (this.cvs.y/2)}px`
+                this.canvas.style.left = `-${(this.stitchedTilesSize.x/2) - (this.cvs.x/2)}px`
+    
                 let tile = new Image()
 
-                // see comment about row placement below
                 let self = this
-                // get canvas coordinates 
+
+                // when the image finishes loading (after we set src)
                 tile.addEventListener(
                     'load',
                     () => {
+                        // convert global tile coords to local ones, then convert to pixels (*128)
                         let canvas_x  = ((x - topleft_x) / 2)*128
                         let canvas_y  = ((topleft_y - y) / 2)*128
+
+                        // draw on the canvas
                         self.ctx.drawImage(
                             tile, 
                             canvas_x,
@@ -88,8 +82,9 @@ class MapView {
                             128
                         )
 
-                        // final iteration
+                        // on final iteration of the loop
                         if(x >= bottomright_x && y <= bottomright_y) {
+                            // this will trigger our draw code below
                             self.assetsLoaded = true
                         }
                     },
@@ -97,11 +92,8 @@ class MapView {
                 )
 
                 tile.src = `http://minecraft.netsoc.co/maps/survival/tiles/world/t/3_1/z_${x}_${y}.png`
-
             }
         }
-
-        //http = new XMLHttpRequest()
     }
     
     getCurrentViewportSize() {
@@ -112,51 +104,75 @@ class MapView {
     }
 
     draw() {
-        
         if(this.assetsLoaded && !this.shouldAnimate) {
             // So, putImageData/getImageData only works on pixels that are displayed on the visible canvas
             // this means we cannot copy our stitched image from above
             // (if we wanted to have the image draw from the center of the viewport)
             // so we're just gonna manipulate it with CSS positioning instead
 
+            this.cvs = this.getCurrentViewportSize()
+
+            // get a random point to interpolate to (and make sure the current screen res will fit in it too)
             this.targetPoint = {
-                x: Math.floor(Math.random()*this.tilesDrawWidth),
-                y: Math.floor(Math.random()*this.tilesDrawHeight)
+                x: (this.cvs.x/2)+Math.floor(Math.random()*(this.stitchedTilesSize.x-this.cvs.x)),
+                y: (this.cvs.y/2)+Math.floor(Math.random()*(this.stitchedTilesSize.y-this.cvs.y))
             }
-            console.log(this.targetPoint)
+
+            // we're gonna start from the center of the stitched tile image
             this.currentPoint = {
-                x: this.tilesDrawWidth / 2,
-                y: this.tilesDrawHeight / 2
+                x: this.stitchedTilesSize.x / 2,
+                y: this.stitchedTilesSize.y / 2
             }
-            this.ratio = 0
+
+            // trigger animation from now on
             this.shouldAnimate = true
+
+            // also fade it in
             this.canvas.className += " visible";
         }
 
         if(this.shouldAnimate) {
+            // position it based on the point we're going to
             this.cvs = this.getCurrentViewportSize()
             this.canvas.style.top = `-${(this.currentPoint.y) - (this.cvs.y/2)}px`
             this.canvas.style.left = `-${(this.currentPoint.x) - (this.cvs.x/2)}px`
     
+            // if we're more than 185px away
+            if(Math.sqrt(
+                Math.pow(this.targetPoint.x-this.currentPoint.x,2) + 
+                Math.pow(this.targetPoint.y - this.currentPoint.y,2)) > 185) {
+     
+                // bezier interpolate to our target
+                this.currentPoint = bezier(
+                    this.currentPoint, 
+                    // I thought I could use a control point with a slight x,y offset,
+                    // unfortunately this pushes the canvas out of bounds :(
+                    { x: this.currentPoint.x, y: this.currentPoint.y }, 
+                    this.targetPoint, 0.025
+                )
 
-            if(this.ratio < 0.98) {
-                console.log('beziering')
-                this.currentPoint = bezier(this.currentPoint, 
-                    { x: this.currentPoint.x + 20, y: this.currentPoint.y - 20} , 
-                        this.targetPoint, this.ratio)
-                this.ratio += 0.0001
             } else {
+                // if we reached our destination 
 
+                // pick a new random (make sure its a good distance away like 250px
+                while(Math.sqrt(
+                    Math.pow(this.targetPoint.x-this.currentPoint.x,2) + 
+                    Math.pow(this.targetPoint.y - this.currentPoint.y,2)) < 350) {
+                    
+                    this.targetPoint = {
+                        x: (this.cvs.x/2)+Math.floor(Math.random()*(this.stitchedTilesSize.x-this.cvs.x)),
+                        y: (this.cvs.y/2)+Math.floor(Math.random()*(this.stitchedTilesSize.y-this.cvs.y))
+                    }
+                }
             }
         }
 
         window.requestAnimationFrame(mapView.draw.bind(this))
     }
-
 }
 
+// bezier interp between 2 x,y coordinates with a control point
 function bezier(startXY, controlXY, endXY, ratio) {
-    // i imple
     return {
         x: Math.pow((1-ratio),2) * startXY.x + (2 * (1-ratio) * ratio * controlXY.x) + Math.pow(ratio,2) * endXY.x,
         y: Math.pow((1-ratio),2) * startXY.y + (2 * (1-ratio) * ratio * controlXY.y) + Math.pow(ratio,2) * endXY.y
